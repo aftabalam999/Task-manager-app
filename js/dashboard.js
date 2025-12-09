95a8ca440eb95e17c0b98c3506e4b37346e28a05
aftabalam999 <course.aftabalam@gmail.com>
2025-12-09 06:27:18 +0530

    task saved in localstorage

--- js/dashboard.js
+++ js/dashboard.js
@@ -23,19 +23,45 @@ addTaskSection.cancelBtn.addEventListener('click', ()=>{
 })
 const ul = document.querySelector("#ul");
 
+// Load from LocalStorage
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+tasks.forEach((task, index) => {
+    const li = createElement(task.title, task.description, index + 1);
+    ul.appendChild(li);
+});
+
 addTaskSection.addTaskBtn.addEventListener('click', ()=>{
     const titlevalue = addTaskSection.title.value;
     const descripvalue = addTaskSection.description.value;
-    // addtasks(titlevalue,descripvalue);
-    ul.innerHTML=`<li>hello</li>`;
+    if(titlevalue && descripvalue){
+        addtasks(titlevalue,descripvalue);
+        addTaskSection.title.value = "";
+        addTaskSection.description.value = "";
+        addTaskSection.mainContainer.classList.add('hidden');
+    } else {
+        alert("Please enter title and description");
+    }
 })
 
 function addtasks(title,description){
-    console.log(title);
-    console.log(description);
+    // Save to LocalStorage
+    tasks.push({ title, description });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
+    const index = ul.children.length + 1;
+    const li = createElement(title, description, index);
+    ul.appendChild(li);
 }
 
-// function createElement(){
-//     let div 
-// }
+function createElement(title, description, index){
+    let li = document.createElement('li');
+    li.classList.add('task-con');
+    li.innerHTML = `
+        <div class="task">
+            <h3>#${index} ${title}</h3>
+            <p>${description}</p>
+        </div>
+    `;
+    return li;
+}
+
